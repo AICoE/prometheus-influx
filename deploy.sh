449a46d915787d7c43594c99f2057feb4c18f23d
Pavel Kajaba <pkajaba@redhat.com>
2018-03-08 17:23:06 +0100

    Changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,17 @@
+#!/bin/bash -e
 
-oc create -f node-exporter.yaml -n kube-system
-oc adm policy add-scc-to-user -z prometheus-node-exporter -n kube-system hostaccess
-oc annotate ns kube-system openshift.io/node-selector= --overwrite
+PROJECT_NAME=${PROJECT_NAME:-"aiops"}
+
+# deploy prometheus
+oc process -p NAMESPACE=$PROJECT_NAME -f  aiops-prometheus.yaml | oc apply -f -
+
+# deploy prometheus-remote-storage
+oc process -f prometheus-remote-storage-adapter.yaml | oc apply -f -
+
+# deploy influxdb
+oc process -f influxdb.yaml | oc apply -f -
+
+# deploy node-exporter
+oc create -f node-exporter.yaml -n $PROJECT_NAME
+oc adm policy add-scc-to-user -z prometheus-node-exporter -n $PROJECT_NAME hostaccess
+oc annotate ns $PROJECT_NAME openshift.io/node-selector= --overwrite
